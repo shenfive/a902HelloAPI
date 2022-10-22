544e19cb39cd7548a6252a3075367b40cee6b874
Danny Shen <shenfive@gmail.com>
2022-10-22 13:54:22 +0800

    get data

--- a902HelloAPI/APIModel.swift
+++ a902HelloAPI/APIModel.swift
@@ -8,5 +8,33 @@ import Alamofire
 import Foundation
 
 class APIModel{
+    static var share = APIModel()
+    private init(){}
+    
+    private var server = "https://randomuser.me/"
+
+    func queryRandomUser(completion:@escaping (_ Data:Any?,_ respError: Error?)->())->(){
+        let apiUrlString = server + "api/"
+        let parameters:Parameters? = nil
+        
+        AF.request(apiUrlString,
+                   method: .get,
+                   parameters: parameters,
+                   encoding: URLEncoding.default,
+                   headers: nil)
+        .responseJSON { response in
+            switch response.result{
+            case .success(_):
+                completion(response.data,nil)
+            case .failure(let error):
+                completion(nil,error)
+            }
+        }
+        
+    }
+
+    
+    
+    
     
 }
--- a902HelloAPI/ViewController.swift
+++ a902HelloAPI/ViewController.swift
@@ -11,9 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        callAPI()
+        
     }
 
+    func callAPI(){
+        APIModel.share.queryRandomUser { rowData, respError in
+            if let respError = respError{
+                print(respError.localizedDescription)
+                return
+            }
+            
+            if let data = rowData as? Data{
+                
+                let str = String(decoding: data, as: UTF8.self)
+                print("===============")
+                print(str)
+                print("===============")
+            }
+            
+            //我們要處理 JSON
+            
+        }
+    }
 
 }
 
